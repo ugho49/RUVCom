3905a0df79a6c4848fae96a46eaaa9ad048e3a11
Ronan LEGARDINIER <ronan.legardinier@gmail.com>
2016-01-20 18:50:41 +0100

    Add onclick sendMessageListener

--- app/src/main/java/fr/nantes/iut/ruvcom/Activities/ConversationActivity.java
+++ app/src/main/java/fr/nantes/iut/ruvcom/Activities/ConversationActivity.java
@@ -4,19 +4,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import fr.nantes.iut.ruvcom.Bean.User;
 import fr.nantes.iut.ruvcom.R;
 
-public class ConversationActivity extends AppCompatActivity {
+public class ConversationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    User user;
+    User distantUser;
+
+    private ImageButton sendButton;
+    private EditText message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
-        //User user = (User) getIntent().getSerializableExtra("user");
-        User distantUser = (User) getIntent().getSerializableExtra("distantUser");
+        user = (User) getIntent().getSerializableExtra("user");
+        distantUser = (User) getIntent().getSerializableExtra("distantUser");
+
+        sendButton = (ImageButton) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(this);
+
+        message = (EditText) findViewById(R.id.message);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(distantUser.getDisplayName());
@@ -39,5 +55,15 @@ public class ConversationActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sendButton:
+                Toast.makeText(getBaseContext(), user.getDisplayName() + " to " + distantUser.getDisplayName() + " : " + message.getText(), Toast.LENGTH_LONG).show();
+                message.setText("");
+                break;
+        }
     }
 }
