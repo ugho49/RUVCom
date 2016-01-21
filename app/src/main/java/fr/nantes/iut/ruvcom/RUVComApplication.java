865ecda92bb5615164c937de1e3f4470b7ad8d09
Ugho Stephan <stephan.ugho@gmail.com>
2016-01-21 08:13:24 +0100

    meilleur gestion cache photo

--- app/src/main/java/fr/nantes/iut/ruvcom/RUVComApplication.java
+++ app/src/main/java/fr/nantes/iut/ruvcom/RUVComApplication.java
@@ -1,16 +1,11 @@
 package fr.nantes.iut.ruvcom;
 
-import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
-import android.os.Build;
-import android.os.StrictMode;
-import android.provider.SyncStateContract;
 
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 /**
  * Created by ughostephan on 19/01/2016.
@@ -25,19 +20,15 @@ public class RUVComApplication extends Application {
     }
 
     public static void initImageLoader(Context context) {
-        // This configuration tuning is custom. You can tune every option, you may tune some of them,
-        // or you can create default configuration by
-        //  ImageLoaderConfiguration.createDefault(this);
-        // method.
-        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
-        config.threadPriority(Thread.NORM_PRIORITY - 2);
-        config.denyCacheImageMultipleSizesInMemory();
-        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
-        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
-        config.tasksProcessingOrder(QueueProcessingType.LIFO);
-        //config.writeDebugLogs(); // Remove for release app
+        // Create global configuration and initialize ImageLoader with this configuration
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
 
-        // Initialize ImageLoader with configuration.
-        ImageLoader.getInstance().init(config.build());
+        ImageLoader.getInstance().init(config);
     }
 }
